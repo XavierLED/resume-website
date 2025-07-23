d9b27915cec725899a9ccc7122971cebbf7bd060
Xavier Bester <xavierbester6@gmail.com>
2025-07-23 16:52:21 -0600

    you can now go to Home too

--- blueprint/home/route.py
+++ blueprint/home/route.py
@@ -3,5 +3,5 @@ from flask import request, render_template, redirect, url_for, Blueprint
 home = Blueprint('home', __name__)
 
 @home.route('/')
-def runProjects():
+def runHome():
     return render_template('home/home.html')
