a2db3ea645e616d52d5238740eca1d27785639a0
Xavier Bester <xavierbester6@gmail.com>
2025-07-24 16:53:17 -0600

    changing different colour

--- blueprint/app.py
+++ blueprint/app.py
@@ -6,11 +6,9 @@ def createApp():
     from blueprint.resume.route import resume
     from blueprint.projects.routes import projects
     from blueprint.home.route import home
-    from blueprint.contact.route import contact
     
     app.register_blueprint(home, url_prefix='/')
     app.register_blueprint(projects, url_prefix='/projects')
-    app.register_blueprint(contact, url_prefix='/contact')
     app.register_blueprint(resume, url_prefix='/resume')
     return app
 
