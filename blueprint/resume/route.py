bd4bdb82fabc1d00db78ac9923f2aa843dddf4e6
Xavier Bester <xavierbester6@gmail.com>
2025-07-24 12:22:17 -0600

    adding pdf for my resume

--- blueprint/resume/route.py
+++ blueprint/resume/route.py
@@ -1,7 +1,7 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, send_from_directory
 
 resume = Blueprint('resume', __name__)
 
 @resume.route('/')
 def runResume():
-    return render_template('resume/resume.html')
+    return send_from_directory('static/pdf', "Xaviers Resume.pdf")
