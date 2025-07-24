d145f807b452470601e386ba51918d77ca7c9c4a
Xavier Bester <xavierbester6@gmail.com>
2025-07-24 14:39:48 -0600

    changing up wording ext ext

--- blueprint/contact/route.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template, Blueprint
-
-contact = Blueprint('contact', __name__)
-
-@contact.route('/')
-def runContact():
-    return render_template('contact/contact.html')
