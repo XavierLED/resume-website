d34c2eb7251a805b6ec733a0122fe5de50df70ba
Xavier Bester <xavierbester6@gmail.com>
2025-07-26 23:08:31 -0600

    adding idea/proof of concept

--- blueprint/projects/routes.py
+++ blueprint/projects/routes.py
@@ -5,3 +5,7 @@ projects = Blueprint('projects', __name__)
 @projects.route('/')
 def runProjects():
     return render_template('projects/projects.html')
+
+@projects.route('/resume-website')
+def runWebsite():
+    return render_template('projects/website.html')
