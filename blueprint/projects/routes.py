9808cb935417254d8e4778fe9db410d5c58190a7
Xavier Bester <xavierbester6@gmail.com>
2025-07-26 23:11:37 -0600

    adding all the flask functions to make the paths be possible with url for call

--- blueprint/projects/routes.py
+++ blueprint/projects/routes.py
@@ -9,3 +9,34 @@ def runProjects():
 @projects.route('/resume-website')
 def runWebsite():
     return render_template('projects/website.html')
+
+@projects.route('/fitness-tracker')
+def runFitnessTracker():
+    return render_template('projects/fitnessTracker.html')
+
+@projects.route('/connect4')
+def runConnect():
+    return render_template('projects/connect4.html')
+
+@projects.route('/todo-go')
+def runTodo():
+    return render_template('projects/goTodo.html')
+
+@projects.route('/tik-tak-toe')
+def runTikTakToe():
+    return render_template('projects/tikTakToe.html')
+
+@projects.route('/tumor-detection')
+def runTumor():
+    return render_template('projects/tumorDetection.html')
+
+@projects.route('/weather-app')
+def runWeather():
+    return render_template('projects/weatherapp.html')
+
+@projects.route('/wiki-scraper')
+def runWikiScraper():
+    return render_template('projects/wikiScraper.html')
+
+
+
